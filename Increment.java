be2b87fc6bd9679c64ea90723aca5359f15af2fd
Wafa AlKebsi <wafa.alkebsi@udc.edu>
2017-12-15 09:15:11 -0500

    Create Increment.java

---
+++ Increment.java
@@ -0,0 +1,13 @@
+# Java-Incerement-
+This is a Java array will incerements every elements [1,2,3] by one
+
+public class Program {
+    public static void main(String[] args) {
+
+// Loop will run through 1, 2, 3 and increment by 1, until it reaches 4, system stops. 
+
+        for (int i = 1; i < 4; i++) {
+            System.out.println(i);
+        }
+    }
+}
