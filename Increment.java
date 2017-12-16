a10fc9b67508b069d757211b9e30fb7d0fc7195e
Wafa AlKebsi <wafa.alkebsi@udc.edu>
2017-12-16 13:14:16 -0500

    Update Increment.java

--- Increment.java
+++ Increment.java
@@ -1,10 +1,10 @@
 # Java-Incerement-
-This is a Java array will incerements every elements [1,2,3] by one
+// This is a Java array will incerements every element in the array [1,2,3] by one
 
 public class Program {
     public static void main(String[] args) {
 
-// Loop will run through 1, 2, 3 and increment by 1, until it reaches 4, system stops. 
+// This Loop will run through 1, 2, 3 and increment by 1, until it reaches 4, system pauses 
 
         for (int i = 0; i < 4; i++) {
             System.out.println(i);
