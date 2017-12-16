e5cf169ce82b803704ab95a92598790e4ca24081
Wafa AlKebsi <wafa.alkebsi@udc.edu>
2017-12-16 13:37:50 -0500

    Update Increment.java

--- Increment.java
+++ Increment.java
@@ -2,7 +2,9 @@
 // This is a Java array will incerements every element in the array [1,2,3] by one
 
 public class Incrememt {
-    public static void main(String[1,2,3] args) {
+    public static void main(String [] args) {
+
+int Array= new int [1,2,3] 
 
 // This Loop will run through 1, 2, 3 and increment by 1, until it reaches 4, system pauses 
 
